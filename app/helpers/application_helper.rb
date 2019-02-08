175b0336359659c0822dffb85a226ee6d997495f
EC2 Default User <ec2-user@ip-172-31-41-137.us-east-2.compute.internal>
2019-02-08 08:30:46 +0000

    modify layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title = '俳句で繋がる')
+        base_title = "俳句で繋がる"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root 'posts#home'
   resources :posts
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
