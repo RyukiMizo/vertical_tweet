c67bbb42f421f7cdc62e33d398dc1181e7206726
EC2 Default User <ec2-user@ip-172-31-41-137.us-east-2.compute.internal>
2019-02-09 07:32:52 +0000

    add sign up structure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190208044509) do
+ActiveRecord::Schema.define(version: 20190209032915) do
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20190208044509) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "password"
+    t.string "password_confirmation"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
