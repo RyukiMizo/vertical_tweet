6502da2d54e092b9f1a05b6802f4e199a07a1f89
Ryuki <ec2-user@ip-172-31-41-137.us-east-2.compute.internal>
2019-03-09 06:07:51 +0000

    modify production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-　config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'haikubeta.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
